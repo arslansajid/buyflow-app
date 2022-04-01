3cf69c8285eca10d70f0e6cbdbbc3f83d60dfa90
Arslan Sajid <arslansajid951@gmail.com>
2022-04-01 22:51:28 +0200

    Integrated context API for data handling

---
+++ src/context/AppContext.tsx
@@ -0,0 +1,64 @@
+import React, { ReactElement, createContext, useCallback, useState } from "react";
+
+export type UpdateContextType = (key: string, value: string | number) => void;
+
+export interface InitalStateInterface {
+  firstName: string,
+  lastName: string,
+  email: string,
+  age: number,
+}
+
+export interface AppContextInterface {
+  data: InitalStateInterface,
+  updateData: UpdateContextType,
+  resetData: () => void,
+}
+export type AppContextValues = undefined | AppContextInterface;
+
+const Context = createContext<AppContextValues>(undefined);
+
+interface ContextProps {
+  children: (ReactElement | null | false)[];
+}
+
+const initialState = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  age: 0,
+}
+
+export const AppContextProvider: React.FC<ContextProps> = (props) => {
+  const [data, setData] = useState<InitalStateInterface>(initialState);
+
+  const updateData = useCallback((key: string, value: string | number) => {
+    setData(prevState => ({
+      ...prevState,
+      [key]: value,
+    }));
+  }, []);
+
+  const resetData = useCallback(() => {
+    setData({ ...initialState });
+  }, []);
+
+  const providerValue = React.useMemo(
+    () => ({
+      data,
+      updateData,
+      resetData
+    }),
+    [
+      data,
+      updateData,
+      resetData
+    ]
+  );
+
+  return (
+    <Context.Provider value={providerValue}>{props.children}</Context.Provider>
+  );
+};
+
+export default Context;
